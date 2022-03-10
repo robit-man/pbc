22c7e34f4ed807a914bd2c7b2e90c8b694edfcb9
robit <36677806+robit-man@users.noreply.github.com>
2022-03-10 15:11:56 -0800

    Update Home.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -201,7 +201,7 @@ header > h1{
 }
 .wrapper{padding:1rem;}
 
-header > svg{transform:rotate(0deg);width:auto;height:auto;}
+header > svg{transform:rotate(0deg);width:33%;height:auto;}
 
 .rotated{display:flex;flex-flow:column;transform:rotate(0deg);margin:auto;transition:all 0.2s ease;}
 .line{height:100px;clip-path: polygon(0 0, 100% 0%, 100% 0rem, 100% 100%, 2rem 100%, 0 calc(100% - 2rem));}
