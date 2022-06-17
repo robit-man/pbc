6b8c8151a689cecacee58c367d8ce2807ecc8b10
robit <36677806+robit-man@users.noreply.github.com>
2022-06-17 11:29:34 -0700

    Update Home.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -73,12 +73,12 @@ import { Router, Link, Route } from "svelte-routing";
             <div class="button-row">
                 <a href="https://www.prnewswire.com/news-releases/spacex-to-launch-doge-1-to-the-moon-301287016.html"><button>DOGE-1</button></a><div class="crosshair" style="margin-left:0.3rem;"></div>
                 <a href="https://xiprotocol.io/"><button>XI PROTOCOL</button></a>
-                <a href="https://noclip.org/"><button>NOCLIP</button></a>
-                <a href="https://revest.finance/"><button>REVEST</button></a><div class="crosshair" style="margin-left:0.3rem;"></div>
-                <a href="https://rena.finance"><button>RENA</button></a>
-                <a href="https://www.olympusdao.finance/"><button>OLYMPUS</button></a>
-                <a href="https://www.guranft.com/"><button>GURA NFT</button></a>        <div class="crosshair" style="margin-left:0.3rem;"></div>
+                <a href="https://noclip.org/"><button>NOCLIP</button></a><div class="crosshair" style="margin-left:0.3rem;"></div>
+                <a href="https://www.guranft.com/"><button>GURA NFT</button></a> 
+                <a href="https://rena.finance"><button>RENA</button></a>  <div class="crosshair" style="margin-left:0.3rem;"></div>
                 <a href="https://twitter.com/POINTBLANK_LLC" > <button>TWITTER</button> </a>
+                <!--<a href="https://revest.finance/"><button>REVEST</button></a>
+                <a href="https://www.olympusdao.finance/"><button>OLYMPUS</button></a>     -->
               </div>    
     </article>
 </div>
